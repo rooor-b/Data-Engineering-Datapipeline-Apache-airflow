109040be9f4f76f18ebc20b2c231853946424698
unknown <rasha.bahbouh@edigitpath.com>
2020-09-23 10:02:30 +0300

    implement load fact and dimension operators

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -6,17 +6,47 @@ class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
 
+    append_sql="""
+                    BEGIN;
+                    INSERT INTO {table}
+                    {sql};
+                    COMMIT;"""
+    
+    replace_sql="""
+                    BEGIN;
+                    TRUNCATE TABLE {table}; 
+                    INSERT INTO {table}
+                    {sql};
+                    COMMIT;"""
+    
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql_stmt="",
+                 is_append=False,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_stmt = sql_stmt
+        self.is_append = is_append
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info('LoadDimensionOperator Start Executing')
+        
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        insert_sql = ""
+        self.log.info(f"Loading dimension table {self.table}")
+        
+        if self.is_append :
+            insert_sql = self.append_sql.format(table=self.table,sql= self.sql_stmt)
+        else:
+            insert_sql = self.replace_sql.format(table= self.table,sql= self.sql_stmt)
+        
+        self.log.info(f"Inserting into dimension table {self.table} as {insert_sql}")
+        redshift.run(insert_sql)
+        
+        self.log.info(f"Inserting into dimension table {self.table} finished")
